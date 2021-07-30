ff0773bf6f5788621931e8c88e0916e9e44fa28f
Grian <grianguy32@gmail.com>
2021-07-30 06:32:54 +0300

    typing

--- mod.ts
+++ mod.ts
@@ -1,17 +1,31 @@
-interface INIObject {
+// the user facing ini object
+interface UserINIObject {
     parse(str: string): object,
     stringify(obj: object): string,
 }
 
-function parseFunc(str: string): object {
+// internal ini variable object, its what is used for vars
+interface INIVariableObject {
+    [name: string]: number | string | null
+}
+
+// internal INI object, its what parseFunc() parses to.
+interface INIObject {
+    [section: string]: INIVariableObject
+}
+
+function parseFunc(str: string): INIObject {
     return {};
 }
 
-function stringifyFunc(obj: object): string {
+function stringifyFunc(obj: INIObject): string {
     return "";
 }
 
-export const INI: INIObject = {
+const INI: UserINIObject = {
     parse: parseFunc,
     stringify: stringifyFunc,
 }
+
+// doesn't let me export default a const inline, unsure why.
+export default { INI };
